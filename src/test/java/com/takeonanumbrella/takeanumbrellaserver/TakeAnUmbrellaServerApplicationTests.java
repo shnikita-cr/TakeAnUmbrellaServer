5c75ae8d394facd0ae8b784a14945a3c5d71974d
nikita <professornikson@gmail.com>
2024-11-19 01:55:52 +0300

    Initial Spring boot project

---
+++ src/test/java/com/takeonanumbrella/takeanumbrellaserver/TakeAnUmbrellaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.takeonanumbrella.takeanumbrellaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeAnUmbrellaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
